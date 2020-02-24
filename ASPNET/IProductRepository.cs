d8c4af4eae4374c08c919535fb406f06f7717479
morganBarnes <noki9551@gmail.com>
2020-02-24 13:50:26 -0600

    total complete

---
+++ ASPNET/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using ASPNET.Models;
+
+
+namespace ASPNET
+
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+    }
+}
---
+++ ASPNET/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using ASPNET.Models;
+using Dapper;
+
+namespace ASPNET
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepository(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM Products;");
+        }
+
+        public Product GetProduct(int id)
+        {
+            return (Product)_conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id",
+            new { id = id });
+
+        }
+    }
+}
