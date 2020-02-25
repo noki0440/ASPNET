826fa01e98a2fccf3d26680917032426002e69ca
morganBarnes <noki9551@gmail.com>
2020-02-25 10:00:24 -0600

    completed step 4, CreateProduct

--- ASPNET/Models/Product.cs
+++ ASPNET/Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace ASPNET.Models
 {
     public class Product
@@ -9,5 +10,6 @@ namespace ASPNET.Models
         public int CategoryID { get; set; }
         public int OnSale { get; set; }
         public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
     }
 }
--- ASPNET/ProductRepository.cs
+++ ASPNET/ProductRepository.cs
@@ -22,7 +22,7 @@ namespace ASPNET
 
         public Product GetProduct(int id)
         {
-            return (Product)_conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id",
+            return (Product)_conn.QuerySingle<Product>("SELECT * FROM Products WHERE ProductID = @id",
             new { id = id });
         }
 
@@ -30,6 +30,27 @@ namespace ASPNET
         {
             _conn.Execute("UPDATE Products SET Name = @name, Price = @price WHERE ProductID = @id;",
             new { name = product.Name, price = product.Price, id = product.ProductID });
+        }
+
+
+        public void InsertProduct(Product productToInsert)
+        {
+            _conn.Execute("INSERT INTO products(Name, Price, CategoryID) VALUES (@name, @price, @categoryID);",
+                new { name = productToInsert, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _conn.Query<Category>("SELECT * FROM categories;");
+        }
+
+        public Product AssignCategory()
+        {
+            var categoryList = GetCategories();
+            var product = new Product();
+            product.Categories = categoryList;
+
+            return product;
         }
 
         
